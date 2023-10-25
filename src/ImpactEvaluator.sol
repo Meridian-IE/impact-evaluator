ad4142fdc57cf9c59e1f1ee55b52c94c8436a97c
Julian Gruber <julian@juliangruber.com>
2023-10-25 12:08:34 -0700

    Optimize gas fees (#46)
    * optimize round.end gas
    
    * optimize maybeAdvanceRound gas
    
    * optimize currentRoundIndex gas
    
    * optimize deleteOpenRound gas
    
    * optimize reward gas

--- src/ImpactEvaluator.sol
+++ src/ImpactEvaluator.sol
@@ -6,7 +6,6 @@ pragma solidity ^0.8.19;
 contract ImpactEvaluator is AccessControl {
     struct Round {
         uint index;
-        uint end;
         uint totalScores;
     }
 
@@ -14,6 +13,8 @@ contract ImpactEvaluator is AccessControl {
     uint public nextRoundLength = 10;
     uint public roundReward = 100 ether;
     uint64 public constant MAX_SCORE = 1e15;
+    uint public currentRoundIndex;
+    uint public currentRoundEnd;
 
     event MeasurementsAdded(
         string cid,
@@ -37,25 +38,19 @@ contract ImpactEvaluator is AccessControl {
     function advanceRound() private {
         uint nextRoundIndex = openRounds.length == 0
             ? 0
-            : currentRoundIndex() + 1;
+            : currentRoundIndex + 1;
         Round memory round = Round(
             nextRoundIndex,
-            block.number + nextRoundLength,
             0
         );
+        currentRoundEnd = block.number + nextRoundLength;
+        currentRoundIndex = nextRoundIndex;
         openRounds.push(round);
         emit RoundStart(nextRoundIndex);
     }
 
     function adminAdvanceRound() public onlyAdmin {
         advanceRound();
-    }
-
-    function maybeAdvanceRound() private {
-        uint currentRoundEnd = getOpenRound(currentRoundIndex()).end;
-        if (block.number >= currentRoundEnd) {
-            advanceRound();
-        }
     }
 
     function setNextRoundLength(uint _nextRoundLength) public onlyAdmin {
@@ -68,9 +63,11 @@ contract ImpactEvaluator is AccessControl {
     }
 
     function addMeasurements(string memory cid) public virtual returns (uint) {
-        maybeAdvanceRound();
-        emit MeasurementsAdded(cid, currentRoundIndex(), msg.sender);
-        return currentRoundIndex();
+        if (block.number >= currentRoundEnd) {
+            advanceRound();
+        }
+        emit MeasurementsAdded(cid, currentRoundIndex, msg.sender);
+        return currentRoundIndex;
     }
 
     function setScores(
@@ -82,39 +79,31 @@ contract ImpactEvaluator is AccessControl {
             addresses.length == scores.length,
             "Addresses and scores length mismatch"
         );
-        require(roundIndex < currentRoundIndex(), "Round not finished");
+        require(roundIndex < currentRoundIndex, "Round not finished");
 
-        Round storage round = getOpenRound(roundIndex);
+        (uint openRoundsIndex, Round storage round) = getOpenRound(roundIndex);
         uint sumOfScores = validateScores(scores, round.totalScores);
         reward(addresses, scores);
         round.totalScores += sumOfScores;
 
         if (round.totalScores == MAX_SCORE) {
-            deleteOpenRound(roundIndex);
+            deleteOpenRound(openRoundsIndex);
         }
     }
 
     function getOpenRound(
         uint roundIndex
-    ) private view returns (Round storage) {
+    ) private view returns (uint index, Round storage) {
         for (uint i = 0; i < openRounds.length; i++) {
             if (openRounds[i].index == roundIndex) {
-                return openRounds[i];
+                return (i, openRounds[i]);
             }
         }
         revert("Open round does not exist");
     }
 
-    function deleteOpenRound(uint roundIndex) private {
-        // Find index inside `openRounds` and remove it while shrinking
-        // the array.
-        uint openRoundsIndex;
-        for (uint i = 0; i < openRounds.length; i++) {
-            if (openRounds[i].index == roundIndex) {
-                openRoundsIndex = i;
-                break;
-            }
-        }
+    function deleteOpenRound(uint openRoundsIndex) private {
+        // Remove the round while shrinking the array.
         for (uint i = openRoundsIndex; i < openRounds.length - 1; i++) {
             openRounds[i] = openRounds[i + 1];
         }
@@ -122,8 +111,9 @@ contract ImpactEvaluator is AccessControl {
     }
 
     function adminDeleteOpenRound(uint roundIndex) public onlyAdmin {
-        require(roundIndex < currentRoundIndex(), "Round not finished");
-        deleteOpenRound(roundIndex);
+        require(roundIndex < currentRoundIndex, "Round not finished");
+        (uint openRoundsIndex, ) = getOpenRound(roundIndex);
+        deleteOpenRound(openRoundsIndex);
     }
 
     function validateScores(
@@ -146,35 +136,19 @@ contract ImpactEvaluator is AccessControl {
         address payable[] memory addresses,
         uint64[] memory scores
     ) private {
-        uint totalAmount = 0;
-        uint[] memory amounts = new uint[](addresses.length);
-
-        for (uint i = 0; i < addresses.length; i++) {
-            uint score = scores[i];
-            uint256 amount = (score * roundReward) / MAX_SCORE;
-            amounts[i] = amount;
-            totalAmount += amount;
-        }
-
-        require(address(this).balance >= totalAmount, "Not enough funds");
-
         for (uint i = 0; i < addresses.length; i++) {
             address payable addr = addresses[i];
             if (addr == 0x000000000000000000000000000000000000dEaD) {
                 // TODO: Shall we emit an event here too?
                 continue;
             }
-            uint256 amount = amounts[i];
+            uint256 amount = (scores[i] * roundReward) / MAX_SCORE;
             if (addr.send(amount)) {
                 emit Transfer(addr, amount);
             } else {
                 emit TransferFailed(addr, amount);
             }
         }
-    }
-
-    function currentRoundIndex() public view returns (uint) {
-        return openRounds[openRounds.length - 1].index;
     }
 
     modifier onlyAdmin() {
