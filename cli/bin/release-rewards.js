55bf6d8bb8d04c5badd7e5b6b0c31b3f8ef2eb53
Julian Gruber <julian@juliangruber.com>
2023-11-08 22:23:30 +0100

    cli: make `release-rewards` more dynamic

--- cli/bin/release-rewards.js
+++ cli/bin/release-rewards.js
@@ -1,4 +1,13 @@
-import { contractWithSigner } from '../index.js'
+import { contract, contractWithSigner } from '../index.js'
 
 const tx = await contractWithSigner.releaseRewards()
 console.log(tx.hash)
+console.log('Awaiting confirmation...')
+await tx.wait()
+console.log('Rewards released')
+console.log('Awaiting transfers...')
+while (true) {
+  const transfersLeft = await contract.participantCountScheduledForTransfer()
+  console.log('Transfers left:', transfersLeft)
+  if (transfersLeft === 0n) break
+}
