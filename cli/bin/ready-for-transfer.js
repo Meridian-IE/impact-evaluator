156ad2506b99db9e951f4444384e1a94958ea1cd
Julian Gruber <julian@juliangruber.com>
2023-12-07 10:39:07 +0100

    cli: improve `ready-for-transfer`

--- cli/bin/ready-for-transfer.js
+++ cli/bin/ready-for-transfer.js
@@ -1,9 +1,13 @@
 import { contract } from '../index.js'
+import { formatEther } from 'ethers'
 
 for (let i = 0;; i++) {
   try {
-    console.log((await contract.readyForTransfer(i)))
+    const address = await contract.readyForTransfer(i)
+    const amount = await contract.rewardsScheduledFor(address)
+    console.log(`${address} FIL ${formatEther(amount)}`)
   } catch (err) {
     break
   }
 }
+process.exit()
