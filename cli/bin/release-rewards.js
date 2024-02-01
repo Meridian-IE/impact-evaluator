fe7f26dc3c445dd8e6b1639be80516c62569de75
Julian Gruber <julian@juliangruber.com>
2024-02-01 12:24:18 +0100

    cli: fix participant count right after rewards release not available

--- cli/bin/release-rewards.js
+++ cli/bin/release-rewards.js
@@ -1,14 +1,21 @@
-import { contract, contractWithSigner } from '../index.js'
+import { contractWithSigner } from '../index.js'
+import { relative, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 const tx = await contractWithSigner.releaseRewards()
 console.log(tx.hash)
 console.log('Awaiting confirmation...')
 await tx.wait()
 console.log('Rewards released')
-console.log('Awaiting transfers...')
-while (true) {
-  const transfersLeft = await contract.participantCountScheduledForTransfer()
-  console.log('Transfers left:', transfersLeft)
-  if (transfersLeft === 0n) break
-}
+
+const bin = relative(
+  process.cwd(),
+  fileURLToPath(new URL('.', import.meta.url))
+)
+console.log('For progress, check')
+console.log()
+console.log(`  $ node ${join(bin, 'participant-count-scheduled-for-transfer.js')}`)
+console.log()
+console.log(`  $ node ${join(bin, 'scheduled-for-transfer.js')}`)
+console.log()
 process.exit()
