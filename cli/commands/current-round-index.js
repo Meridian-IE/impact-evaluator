f32b029ec51440c82a999a7a6ce946f8ef28eee8
Julian Gruber <julian@juliangruber.com>
2024-04-15 13:51:35 +0200

    fix command hanging

--- cli/commands/current-round-index.js
+++ cli/commands/current-round-index.js
@@ -3,4 +3,5 @@ import { createContract } from '../index.js'
 export const currentRoundIndex = async opts => {
   const { contract } = createContract(opts)
   console.log((await contract.currentRoundIndex()).toString())
+  process.exit(0)
 }
